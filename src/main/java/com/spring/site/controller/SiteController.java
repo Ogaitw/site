9ac66963c45238a97292587d6f31c41f1b72f476
Ogaitw <tiagoaugusto2402@gmail.com>
2023-05-31 21:36:14 -0300

    qualquer um

--- src/main/java/com/spring/site/controller/SiteController.java
+++ src/main/java/com/spring/site/controller/SiteController.java
@@ -2,12 +2,17 @@ package com.spring.site.controller;
 
 import com.spring.site.model.Post;
 import com.spring.site.service.SiteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -21,6 +26,28 @@ public class SiteController {
         mv.addObject("posts",posts);
         return mv;
 
+    }
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostsDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = siteService.findById(id);
+        mv.addObject("post",post);
+        return mv;
+
+    }
+    @RequestMapping (value = "/newpost",method = RequestMethod.GET)
+    public String getPostForm() {
+        return "postForm";
+    }
+    @RequestMapping (value = "/newpost",method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatorios foram preenchidos");
+        if (result.hasErrors()){
+        return "redirect:/newpost";
+    }
+    post.setData(LocalDate.now());
+        siteService.save(post);
+        return  "redirect:/posts";
     }
 
 }
